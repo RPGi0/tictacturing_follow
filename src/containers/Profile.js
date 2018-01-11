be7bdec92cabbb77e46ff1d1c329d8503191b201
Rob <rob@llnnlls-iMac.lan>
2018-01-11 13:35:31 -0500

    Ch 04_06, problems with Graphcool 'viewer'

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,15 +1,98 @@
 import React, {Component} from 'react';
+import {Container, Name, GameListHeader, GameList, GameRecord, Column, ColumnLabels } from '../styled/Profile';
+import Relay from 'react-relay/classic';
 
 class Profile extends Component {
+    static defaultProps = {
+        user: {
+            email: 'USER_EMAIL',
+            games: [
+                {
+                    winner: true,
+                    createdAt: '1/10/2018',
+                    id: '0001'
+                },
+                {
+                    winner: true,
+                    createdAt: '1/11/2018',
+                    id: '0002'
+                },
+                {
+                    winner: true,
+                    createdAt: '1/12/2018',
+                    id: '0003'
+                },
+            ]
+        }
+    };
+
+    get records () {
+      return this.props.user.games.map( (game, index) => {
+          return (
+              <GameRecord
+                key={index}
+                index={index}
+              >
+                  <Column>
+                      {(game.winner) ? 'Won!' : "Didn't win"}
+                  </Column>
+                  <Column>
+                    "ROBOT"
+                  </Column>
+                  <Column>
+                    "No"
+                  </Column>
+                  <Column>
+                      {game.createdAt}
+                  </Column>
+              </GameRecord>
+          )
+          })
+    };
 
     render() {
+        let {email} = this.props.user;
         return (
-            <div>
-             <h2>Profile</h2>
-            </div>
+            <Container>
+                <Name>
+                    {email}
+                </Name>
+                <GameList>
+                    <GameListHeader>
+                        My Games
+                    </GameListHeader>
+                <ColumnLabels>
+                    <Column>
+                        Outcome
+                    </Column>
+                    <Column>
+                        Guess
+                    </Column>
+                    <Column>
+                        Guessed Correctly
+                    </Column>
+                    <Column>
+                        Date
+                    </Column>
+                </ColumnLabels>
+                    {this.records}
+                </GameList>
+            </Container>
         )
+
+    };
+}
+
+export default Relay.createContainer(
+    Profile, {
+        fragments: {
+            viewer: () => Relay.QL`
+                fragment on viewer {
+                    user {
+                    id
+                }
+             }
+          `,
+        }
     }
-};
-
-
-export default Profile;
+)
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import {Route, IndexRoute} from 'react-router';
-import Template from '../containers/Templates'
-import Home from '../containers/Home'
-import Profile from '../containers/Profile'
+import Template from '../containers/Template';
+import TicTacToe from '../containers/TicTacToe';
+import Profile from '../containers/Profile';
+import Relay from 'react-relay/classic';
+
+const ViewerQueries = {
+  viewer: () => Relay.QL`query { viewer }`
+};
 
 const createRoutes = () => {
-    return (
-        <Route
-            path='/'
-            component={Template}
-            >
-            <IndexRoute
-            component={Home}
-            />
-            <Route
-                path={'/profile'}
-                component={Profile}
-            />
-        </Route>
-    )
+  return (
+    <Route
+      path='/'
+      component={Template}
+      query={ViewerQueries}
+    >
+      <IndexRoute
+        component={TicTacToe}
+        query={ViewerQueries}
+      />
+      <Route
+        path={'/profile'}
+        component={Profile}
+        query={ViewerQueries}
+      />
+    </Route>
+  )
 };
 
 const Routes = createRoutes();
